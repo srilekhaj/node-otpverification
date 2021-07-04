ff6fd95e0e4c75ff02bd7d316247f1a1485dc132
Srilekha J <56781872+srilekhaj@users.noreply.github.com>
2021-07-04 15:34:59 +0530

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const sgmail = require('@sendgrid/mail');
 const bodyParser = require('body-parser');
-const accountSid ='AC40eee1555fcb9695ae95442f106f886b'
-const authToken = '2136672f5cb163825240d9c2853ad873'
+const accountSid =''
+const authToken = ''
 const route = require('./routes/api');
 const client = require('twilio')(accountSid, authToken);
-
-//const APIKEY = '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'      
+    
 
 sgmail.setApiKey(process.env.SENDGRID_API);
 const app = express();
@@ -15,8 +14,8 @@ const app = express();
 
 app.get('/emailsent',(req,res)=>{
     const msg = {
-        to: 'jsrilekha.17@gmail.com',
-        from: 'jsrilekha.17@gmail.com', // Use the email address or domain you verified above
+        to: 'xxx@gmail.com',
+        from: 'xxx@gmail.com', // Use the email address or domain you verified above
         subject: 'Sending with Twilio SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>'
@@ -29,7 +28,7 @@ app.get('/emailsent',(req,res)=>{
 
 app.get('/otp', (req,res)=>{
     const client = require('twilio')(accountSid, authToken);
-client.verify.services('VAafb26c77efa584dc40152bf14c98bb4a')
+client.verify.services('******')
 .verifications
 .create({to: `+${req.query.phonenumber}`, channel: req.query.channel})
 .then((data) =>{
@@ -45,7 +44,7 @@ console.log(error.message);
 
 app.get("/verify", (req,res)=>{
     client
-    .verify.services('VAafb26c77efa584dc40152bf14c98bb4a')
+    .verify.services('*********')
     .verifications
     .create({
         to: `+${req.query.phonenumber}`,
